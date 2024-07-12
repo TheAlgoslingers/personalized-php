802d03da1122b08003f087a1144475c9944205f7
The Algoslingers <58689583+TheAlgoslingers@users.noreply.github.com>
2024-07-12 20:43:05 +0000

    Add files via upload

--- src/examples/test.php
+++ src/examples/test.php
@@ -1,1 +1,12 @@
+<?php
+require '../class.PersonalizedContent.php';
 
+## require "path/to/vendor/autoload.php";
+
+## use thealgoslingers\PersonalizedContent
+
+// Usage example
+$api_key = 'your_api_key_here';
+$pc = new PersonalizedContent($api_key);
+$user_ip = '8.8.8.8';
+echo $pc->getContent($user_ip);
